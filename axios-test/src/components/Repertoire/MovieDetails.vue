1fe44a79b49b4a6ff08bc40b602e06a7f20d17c8
Piotrekk00 <126890720+Piotrekk00@users.noreply.github.com>
2024-01-07 15:27:00 +0100

    wow

--- axios-test/src/components/Repertoire/MovieDetails.vue
+++ axios-test/src/components/Repertoire/MovieDetails.vue
@@ -20,6 +20,8 @@ const movie = ref({
   cast: '',
   genres: '',
   description: '',
+  posterUrl: '',
+  posterPublicId: '',
 });
 
 const router = useRoute();
@@ -37,25 +39,47 @@ const fetchMovieData = async () => {
   isLoading.value = false;
   }
 };
+
 onMounted(fetchMovieData);
 </script>
-
 
 <template>
   <main>
     <div class="wrapper">
-      <div class="title">
-        <h1>Szczegóły filmu</h1>
-      </div>
 
       <div v-if="fetchError">{{ fetchError }}</div>
       <div v-if="isLoading">Loading...</div>
+      <div class="content" v-else>
 
-      <div v-else>
-        <div class="movie"></div>
-        {{ movie.title }}
+        <div class="left">
+          <div class="title">
+            <h1>{{ movie.title }}</h1>
+          </div>
+            <span>{{ movie.genres }}</span>
+          <div class="iconSection">
+              <div class="clock">
+                <img src="../../assets/img/clock.svg" alt="clockIcon">
+                <span>czas trwania: <br>{{ movie.duration }} min</span>
+              </div>
+              <div class="calendar">
+                <img src="../../assets/img/calendar.svg" alt="calendarIcon">
+                <span>rok premiery (Polska):<br> {{ movie.release.year }}</span>
+              </div>
+          </div>
+          <div class="description">
+            od lat: {{movie.ageRestriction}} / Produkcja: {{movie.release.country}}<br>
+            Reżyseria: {{movie.director}}<br>
+            Obsada: {{movie.cast}}<br>
+          </div>
+          <div class="description">
+            <b>Opis filmu:</b><br>
+            {{ movie.description }}
+          </div>
+        </div>
+        <div class="right">
+          <img src="https://res.cloudinary.com/db8j2d9nv/image/upload/v1704561137/tkoh7qvqzfxs6hmk9rlr.jpg" alt="moviePoster">
+        </div>
       </div>
-
     </div>
   </main>
 </template>
@@ -67,18 +91,65 @@ onMounted(fetchMovieData);
     margin-inline: auto;
   }
 
-  main .title {
-    padding-top: 45px;
-  }
-
   main .title h1 {
-    font-weight: 500;
+    font-weight: 600;
     font-size: 2.3rem;
   }
 
   hr {
     border: 0.5px solid #686868;
     margin: 10px 0;
+  }
+  .wrapper .content{
+    display: flex;
+    justify-content: space-between;
+    padding-top: 45px;
+
+  }
+
+  .content .right img{
+    max-width: 350px;
+    padding-left: 5px;
+  }
+
+  .iconSection{
+    display: flex;
+    gap: 20px;
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+
+  .clock{
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  .calendar{
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  .iconSection img{
+    width: 40px;
+    filter: invert(9%) sepia(82%) saturate(3259%) hue-rotate(260deg) brightness(119%) contrast(100%);
+  }
+
+  .description{
+    line-height: 25px;
+    margin-top: 30px;
+    text-align: justify;
+    max-width: 800px;
+    padding: 8px;
+  }
+
+  @media only screen and (max-width: 1150px) {
+    .wrapper .content{
+      display: block;
+      align-items: center;
+    }
+    .content .right {
+      align-items: center;
+    }
   }
 
 </style>
